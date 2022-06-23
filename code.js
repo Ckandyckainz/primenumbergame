5f8d95385603ed7719e267d3ef9ac0352d3c4f25
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2022-06-22 23:51:35 -0700

    gets colors for blocks

--- code.js
+++ code.js
@@ -58,23 +58,55 @@ function factor(n){
                 done = true;
             }
             if (primes[counter][0] > Math.sqrt(m)) {
-                factors.push(m);
+                let item = 0;
+                for (let i=0; i<primes.length; i++) {
+                    if (primes[i][0] == m) {
+                        item = primes[i];
+                    }
+                }
+                if (item == 0) {
+                    item = [m, [Math.random()*Math.PI*2, Math.random()*0.3+0.6, Math.random()*0.3+0.6]];
+                    primes.push(item);
+                }
+                factors.push(item);
                 done = true;
             }
-            if (m%primes[counter][0] == 0) {
-                m /= primes[counter][0];
-                factors.push(primes[counter][0]);
-            } else {
-                counter ++;
+            if (!done) {
+                if (m%primes[counter][0] == 0) {
+                    m /= primes[counter][0];
+                    factors.push(primes[counter]);
+                } else {
+                    counter ++;
+                }
             }
         }
         return factors;
     }
 }
 
-let primes = [[2, [Math.random()*Math.PI*2, Math.random()*0.3+0.6, Math.random()*0.3+0.6]]];
+function addBlockTexture(n){
+    let factors = factor(n);
+    let color = [0, 1, 1];
+    factors.forEach((factor)=>{
+        while (factor[1][0] < color[0]-Math.PI) {
+            factor[1][0] += Math.PI*2;
+        }
+        while (factor[1][0] > color[0]+Math.PI) {
+            factor[1][0] -= Math.PI*2;
+        }
+        color[0] += factor[1][0]/factors.length;
+        color[1] *= factor[1][1];
+        color[2] *= factor[1][2];
+    });
+    blockTextures[n] = color;
+    return color;
+}
 
-for (let i=0; i<200; i++) {
-    mctx.fillStyle = HSVtoString(Math.PI*2*i/200, Math.random()*0.3+0.6, Math.random()*0.3+0.6, 1);
-    mctx.fillRect(mcw*i/200, 0, mcw*(i+1)/200, mch);
+let primes = [[2, [Math.random()*Math.PI*2, Math.random()*0.3+0.6, Math.random()*0.3+0.6]]];
+let blockTextures = [];
+
+let w = [2, 3, 6, 5, 30];
+for (let i=0; i<w.length; i++) {
+    mctx.fillStyle = HSVtoString(...addBlockTexture(w[i]), 1);
+    mctx.fillRect((i)*mcw/w.length, 0, (i+1)*mcw/w.length, mch);
 }
